fb1b2a60ac87d16f439fa6f4cf1ebfef39270cd9
chavhansonilal <chavhansonilal@gmail.com>
2025-04-02 22:41:39 +0530

    Defined User and Appointment entities in Domain

--- Domain/User.cs
+++ Domain/User.cs
@@ -2,6 +2,10 @@
 {
     public class User
     {
-
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; } // User, Manager
+        public List<Appointment> Appointments { get; set; } = new();
     }
 }
